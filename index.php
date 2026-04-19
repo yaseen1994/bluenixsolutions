b60e354f6905c612af69f620f8de1c5f7d45ffd5
Yaseen Sarang <sarangyaseen@gmail.com>
2026-04-19 22:48:02 +0530

    home page slider updated

--- index.php
+++ index.php
@@ -89,171 +89,18 @@ include 'includes/header.php';
                                 </h4>
                                 <h1 data-animation="slideInRight" data-duration="2s" data-delay=".5s"
                                     class="text-white">
-                                    Excellent It Services
-                                    for Your Success
+                                    We Build Fast, High-Converting Websites That Drive Real Business Growth
                                 </h1>
                                 <p data-animation="slideInRight" data-duration="2s" data-delay=".7s" class="mt-20">
-                                    Consectetur adipiscing
-                                    elit
-                                    aenean scelerisque at augue vitae consequat
-                                    <br>
-                                    quisque eget congue
-                                    velit
-                                    in cursus leo sed
-                                    sodales est eget turpis.
+                                    We design, develop, and optimize websites that generate leads, improve performance,
+                                    and scale your business online.
                                 </p>
                                 <a data-animation="slideInRight" data-duration="2s" data-delay=".9s" href="<?php echo $base_url; ?>/about"
-                                    class="btn-one mt-60">Explore
-                                    More <i class="fa-regular fa-arrow-right-long"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div data-animation="slideInLeft" data-duration="2s" data-delay=".3s"
-                            class="banner__shape-left2">
-                            <img src="<?php echo $base_url; ?>/assets/images/banner/banner-regular-left-shape.png" alt="shape">
-                        </div>
-                        <div data-animation="slideInLeft" data-duration="2s" data-delay=".9s"
-                            class="banner__shape-left1">
-                            <img src="<?php echo $base_url; ?>/assets/images/banner/banner-solid-left-shape.png" alt="shape">
-                        </div>
-                        <div class="banner__shape-left3 wow slideInLeft">
-                            <img class="sway__animation" src="<?php echo $base_url; ?>/assets/images/banner/banner-shape-left.png" alt="shape">
-                        </div>
-                        <div class="banner__shape-right2" data-animation="slideInRight" data-duration="3s"
-                            data-delay=".3s">
-                            <img src="<?php echo $base_url; ?>/assets/images/banner/banner-shape-right-line.png" alt="shape">
-                        </div>
-                        <div class="banner__shape-right1" data-animation="slideInRight" data-duration="2s"
-                            data-delay=".3s">
-                            <img src="<?php echo $base_url; ?>/assets/images/banner/banner-shape-right.png" alt="shape">
-                        </div>
-                        <div class="banner__right-line1" data-animation="slideInRight" data-duration="2s"
-                            data-delay=".9s">
-                            <img src="<?php echo $base_url; ?>/assets/images/banner/banner-right-line1.png" alt="shape">
-                        </div>
-                        <div class="banner__right-line2" data-animation="slideInRight" data-duration="2s"
-                            data-delay=".7s">
-                            <img src="<?php echo $base_url; ?>/assets/images/banner/banner-right-line2.png" alt="shape">
-                        </div>
-                        <div class="banner__right-line3" data-animation="slideInRight" data-duration="2s"
-                            data-delay=".5s">
-                            <img src="<?php echo $base_url; ?>/assets/images/banner/banner-right-line3.png" alt="shape">
-                        </div>
-                        <div class="banner__right-line4" data-animation="slideInRight" data-duration="2s"
-                            data-delay=".3s">
-                            <img src="<?php echo $base_url; ?>/assets/images/banner/banner-right-line4.png" alt="shape">
-                        </div>
-                        <div class="slide-bg" data-background="<?php echo $base_url; ?>/assets/images/banner/banner-image2.jpg"></div>
-                        <div class="container">
-                            <div class="banner__content">
-                                <h4 data-animation="slideInRight" data-duration="2s" data-delay=".3s"
-                                    class="text-white mb-20">
-                                    <svg class="me-1" width="40" height="16" viewBox="0 0 40 16" fill="none"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <rect x="0.5" y="0.500183" width="25.6667" height="15" rx="7.5"
-                                            stroke="white" />
-                                        <rect x="13.3334" y="0.000183105" width="26.6667" height="16" rx="8"
-                                            fill="white" />
-                                    </svg>
-                                    Best it SOULTION Provider
-                                </h4>
-                                <h1 data-animation="slideInRight" data-duration="2s" data-delay=".5s"
-                                    class="text-white">
-                                    Excellent It Services
-                                    for Your Success
-                                </h1>
-                                <p data-animation="slideInRight" data-duration="2s" data-delay=".7s" class="mt-20">
-                                    Consectetur adipiscing
-                                    elit
-                                    aenean scelerisque at augue vitae consequat
-                                    <br>
-                                    quisque eget congue
-                                    velit
-                                    in cursus leo sed
-                                    sodales est eget turpis.
-                                </p>
-                                <a data-animation="slideInRight" data-duration="2s" data-delay=".9s" href="<?php echo $base_url; ?>/about"
-                                    class="btn-one mt-60">Explore
-                                    More <i class="fa-regular fa-arrow-right-long"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div data-animation="slideInLeft" data-duration="2s" data-delay=".3s"
-                            class="banner__shape-left2">
-                            <img src="<?php echo $base_url; ?>/assets/images/banner/banner-regular-left-shape.png" alt="shape">
-                        </div>
-                        <div data-animation="slideInLeft" data-duration="2s" data-delay=".9s"
-                            class="banner__shape-left1">
-                            <img src="<?php echo $base_url; ?>/assets/images/banner/banner-solid-left-shape.png" alt="shape">
-                        </div>
-                        <div class="banner__shape-left3 wow slideInLeft">
-                            <img class="sway__animation" src="<?php echo $base_url; ?>/assets/images/banner/banner-shape-left.png" alt="shape">
-                        </div>
-                        <div class="banner__shape-right2" data-animation="slideInRight" data-duration="3s"
-                            data-delay=".3s">
-                            <img src="<?php echo $base_url; ?>/assets/images/banner/banner-shape-right-line.png" alt="shape">
-                        </div>
-                        <div class="banner__shape-right1" data-animation="slideInRight" data-duration="2s"
-                            data-delay=".3s">
-                            <img src="<?php echo $base_url; ?>/assets/images/banner/banner-shape-right.png" alt="shape">
-                        </div>
-                        <div class="banner__right-line1" data-animation="slideInRight" data-duration="2s"
-                            data-delay=".9s">
-                            <img src="<?php echo $base_url; ?>/assets/images/banner/banner-right-line1.png" alt="shape">
-                        </div>
-                        <div class="banner__right-line2" data-animation="slideInRight" data-duration="2s"
-                            data-delay=".7s">
-                            <img src="<?php echo $base_url; ?>/assets/images/banner/banner-right-line2.png" alt="shape">
-                        </div>
-                        <div class="banner__right-line3" data-animation="slideInRight" data-duration="2s"
-                            data-delay=".5s">
-                            <img src="<?php echo $base_url; ?>/assets/images/banner/banner-right-line3.png" alt="shape">
-                        </div>
-                        <div class="banner__right-line4" data-animation="slideInRight" data-duration="2s"
-                            data-delay=".3s">
-                            <img src="<?php echo $base_url; ?>/assets/images/banner/banner-right-line4.png" alt="shape">
-                        </div>
-                        <div class="slide-bg" data-background="<?php echo $base_url; ?>/assets/images/banner/banner-image3.jpg"></div>
-                        <div class="container">
-                            <div class="banner__content">
-                                <h4 data-animation="slideInRight" data-duration="2s" data-delay=".3s"
-                                    class="text-white mb-20">
-                                    <svg class="me-1" width="40" height="16" viewBox="0 0 40 16" fill="none"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <rect x="0.5" y="0.500183" width="25.6667" height="15" rx="7.5"
-                                            stroke="white" />
-                                        <rect x="13.3334" y="0.000183105" width="26.6667" height="16" rx="8"
-                                            fill="white" />
-                                    </svg>
-                                    Best it SOULTION Provider
-                                </h4>
-                                <h1 data-animation="slideInRight" data-duration="2s" data-delay=".5s"
-                                    class="text-white">
-                                    Excellent It Services
-                                    for Your Success
-                                </h1>
-                                <p data-animation="slideInRight" data-duration="2s" data-delay=".7s" class="mt-20">
-                                    Consectetur adipiscing
-                                    elit
-                                    aenean scelerisque at augue vitae consequat
-                                    <br>
-                                    quisque eget congue
-                                    velit
-                                    in cursus leo sed
-                                    sodales est eget turpis.
-                                </p>
-                                <a data-animation="slideInRight" data-duration="2s" data-delay=".9s" href="<?php echo $base_url; ?>/about"
-                                    class="btn-one mt-60">Explore
-                                    More <i class="fa-regular fa-arrow-right-long"></i></a>
+                                    class="btn-one mt-60">Get a Free Website Audit <i class="fa-regular fa-arrow-right-long"></i></a>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="banner__dot-wrp">
-                <div class="dot-light banner__dot"></div>
             </div>
         </section>
         <!-- Banner area end here -->
